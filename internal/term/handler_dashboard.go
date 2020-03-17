d7cdd7f27b8fbcde37a2b119e2545fa615046d7c
gmajkic <gmajkic@vente-privee.com>
2020-03-17 19:38:21 +0100

    feat(dashboard): improve file structure

--- internal/term/handler_dashboard.go
+++ internal/term/handler_dashboard.go
@@ -1,6 +1,7 @@
 package term
 
 import (
+	"fmt"
 	"log"
 	"math"
 	"os"
@@ -11,19 +12,15 @@ import (
 	"github.com/gizak/termui/v3/widgets"
 )
 
-var (
-	queue         []string
-	previousState os.FileInfo
-	i             int
-	barchartData  []float64
-	bcLabels      []string
-	statistics    map[string]int
-)
+type line struct {
+	date    string
+	section string
+}
 
 // Term blabla
 type Term struct {
 	previousState os.FileInfo
-	queue         []string
+	queue         []line
 	barchartData  []float64
 	bcLabels      []string
 	statistics    map[string]int
@@ -34,13 +31,20 @@ type Term struct {
 	start         time.Time
 }
 
-// Conf blablba
+type dashboard struct {
+	p  *widgets.Plot
+	b  *widgets.BarChart
+	l  *widgets.List
+	pa *widgets.Paragraph
+}
+
+// Conf contain a cli parameter required to run a dashboard
 type Conf struct {
 	Logfile   string
 	Threshold int
 }
 
-// NewTerm blablab
+// NewTerm create a newTerm configuration
 func NewTerm(conf *Conf) *Term {
 	return &Term{
 		logfile:   conf.Logfile,
@@ -49,7 +53,133 @@ func NewTerm(conf *Conf) *Term {
 	}
 }
 
-// Run run
+func (t *Term) makeDashboard() *dashboard {
+	p := widgets.NewParagraph()
+	p.Title = "Alert"
+	p.SetRect(0, 0, 80, 5)
+	p.TextStyle.Fg = ui.ColorWhite
+	p.BorderStyle.Fg = ui.ColorCyan
+
+	// default list value
+	listData := []string{
+		"[0] gizak/termui",
+		"[1] editbox.go",
+		"[2] interrupt.go",
+		"[3] keyboard.go",
+		"[4] output.go",
+		"[5] random_out.go",
+		"[6] dashboard.go",
+		"[7] nsf/termbox-go",
+	}
+
+	l := widgets.NewList()
+	l.Title = "Log"
+	l.Rows = listData
+	l.SetRect(0, 20, 50, 5)
+	l.TextStyle.Fg = ui.ColorYellow
+
+	bc := widgets.NewBarChart()
+	bc.Title = "Pourcentage of each section"
+	bc.SetRect(50, 5, 80, 20)
+	bc.BarWidth = 6
+	bc.LabelStyles = []ui.Style{ui.NewStyle(ui.ColorBlue)}
+	bc.NumStyles = []ui.Style{ui.NewStyle(ui.ColorBlack)}
+
+	// make a slice with a len of 73 corresponding to barchart width
+	t.sinData = make([]float64, 73)
+
+	lc2 := widgets.NewPlot()
+	lc2.Title = "Number of requests"
+	lc2.Data = make([][]float64, 1)
+	lc2.Data[0] = t.sinData
+	lc2.SetRect(0, 20, 80, 30)
+	lc2.AxesColor = ui.ColorWhite
+	lc2.LineColors[0] = ui.ColorYellow
+
+	result := dashboard{b: bc, p: lc2, l: l, pa: p}
+	return &result
+}
+
+func drawBarchart(t *Term) {
+	t.statistics[t.queue[0].section]++
+	match := false
+	pourcent := (float64(t.statistics[t.queue[0].section]) / float64(t.sum)) * 100
+	for k, v := range t.bcLabels {
+		if v == t.queue[0].section {
+			t.barchartData[k] = math.Ceil(pourcent*100) / 100
+			match = true
+		} else {
+			pourcent := (float64(t.statistics[v]) / float64(t.sum)) * 100
+			t.barchartData[k] = math.Ceil(pourcent*100) / 100
+		}
+	}
+	if !match {
+		t.bcLabels = append(t.bcLabels, t.queue[0].section)
+		t.barchartData = append(t.barchartData, math.Ceil(pourcent*100)/100)
+	}
+}
+
+func drawAlert(t *Term, d *dashboard, max int) {
+	current := time.Now()
+	// constraint:
+	// Whenever total traffic for the past 2 minutes exceeds a certain number on average
+	if current.Sub(t.start).Seconds() >= 120 && max > t.threshold {
+		d.pa.Text = "High traffic generated an alert - hits = " + strconv.Itoa(max) + ", triggered at " + current.Format(time.UnixDate)
+		t.start = time.Now()
+		max = 0
+	}
+}
+
+func drawList(d *dashboard, t *Term) {
+	if len(t.queue) != 0 {
+		d.l.Rows = append(d.l.Rows, fmt.Sprint(t.queue[0].date, " - ", t.queue[0].section))
+		d.l.Rows = d.l.Rows[1:]
+	}
+}
+
+func drawLine(d *dashboard, t *Term) {
+	d.p.Data[0] = t.sinData
+	if len(t.sinData) > 72 {
+		t.sinData = t.sinData[2:]
+	} else {
+		t.sinData = append(t.sinData, t.sinData[len(t.sinData)-1])
+	}
+}
+
+// every 10 seconds draw the dashboard
+func drawDashboard(initalStat os.FileInfo, t *Term, d *dashboard, max int) {
+	t.Parse(initalStat)
+	// looking for the max len of queue
+	// if max is upper than threshold, trigger an alert
+	if len(t.queue) > max {
+		max = len(t.queue)
+	}
+
+	if len(t.queue) > 0 {
+		for len(t.queue) > 0 {
+			t.sum++
+
+			drawBarchart(t)
+
+			drawList(d, t)
+
+			t.queue = t.queue[1:]
+		}
+	}
+
+	drawAlert(t, d, max)
+
+	t.sinData = append(t.sinData, float64(t.sum))
+
+	drawLine(d, t)
+
+	d.b.Data = t.barchartData
+	d.b.Labels = t.bcLabels
+
+	ui.Render(d.p, d.l, d.b, d.pa)
+}
+
+// Run dashboard
 func (t *Term) Run() {
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
@@ -63,117 +193,25 @@ func (t *Term) Run() {
 
 	t.statistics = make(map[string]int)
 
-	p := widgets.NewParagraph()
-	p.Title = "Text Box"
-	//p.Text = "PRESS q TO QUIT DEMO"
-	p.SetRect(0, 0, 80, 5)
-	p.TextStyle.Fg = ui.ColorWhite
-	p.BorderStyle.Fg = ui.ColorCyan
+	dashboard := t.makeDashboard()
 
 	updateParagraph := func(count int) {
 		if count%2 == 0 {
-			p.TextStyle.Fg = ui.ColorRed
+			dashboard.pa.TextStyle.Fg = ui.ColorRed
 		} else {
-			p.TextStyle.Fg = ui.ColorWhite
+			dashboard.pa.TextStyle.Fg = ui.ColorWhite
 		}
 	}
 
-	listData := []string{
-		"[0] gizak/termui",
-		"[1] editbox.go",
-		"[2] interrupt.go",
-		"[3] keyboard.go",
-		"[4] output.go",
-		"[5] random_out.go",
-		"[6] dashboard.go",
-		"[7] nsf/termbox-go",
-	}
-
-	l := widgets.NewList()
-	l.Title = "List"
-	l.Rows = listData
-	l.SetRect(0, 20, 50, 5)
-	l.TextStyle.Fg = ui.ColorYellow
-
-	bc := widgets.NewBarChart()
-	bc.Title = "Bar Chart"
-	bc.SetRect(50, 5, 80, 20)
-	bc.BarWidth = 5
-	bc.BarColors[0] = ui.ColorBlue
-
-	t.sinData = make([]float64, 73)
-
-	lc2 := widgets.NewPlot()
-	lc2.Title = "braille-mode Line Chart"
-	lc2.Data = make([][]float64, 1)
-	lc2.Data[0] = t.sinData
-	lc2.SetRect(0, 20, 80, 30)
-	lc2.AxesColor = ui.ColorWhite
-	lc2.LineColors[0] = ui.ColorYellow
-
-	barchartData = nil
-	bcLabels = nil
 	t.sum = 0
 	max := 0
-	draw := func(count int, initalStat os.FileInfo) {
-		t.Parse(initalStat)
-		if len(t.queue) > max {
-			max = len(t.queue)
-		}
-		if len(t.queue) > 0 {
-			t.statistics[t.queue[0]]++
-			t.sum++
-			t.sinData = append(t.sinData, float64(t.sum))
-			match := false
-			pourcent := (float64(t.statistics[t.queue[0]]) / float64(t.sum)) * 100
-			for k, v := range t.bcLabels {
-				if v == t.queue[0] {
-					t.barchartData[k] = math.Ceil(pourcent*100) / 100
-					match = true
-				} else {
-					pourcent := (float64(t.statistics[v]) / float64(t.sum)) * 100
-					t.barchartData[k] = math.Ceil(pourcent*100) / 100
-				}
-			}
-			if !match {
-				t.bcLabels = append(t.bcLabels, t.queue[0])
-				t.barchartData = append(t.barchartData, math.Ceil(pourcent*100)/100)
-			}
-
-			if len(t.queue) != 0 {
-				l.Rows = append(l.Rows, t.queue[0])
-				l.Rows = l.Rows[1:]
-			}
-			t.queue = t.queue[1:]
-
-		}
-		current := time.Now()
-		if current.Sub(t.start).Seconds() >= 120 && max > t.threshold {
-			p.Text = "High traffic generated an alert - hits = " + strconv.Itoa(max) + ", triggered at " + current.Format(time.UnixDate)
-			t.start = time.Now()
-			max = 0
-		}
-
-		lc2.Data[0] = t.sinData
-		if len(t.sinData) > 72 {
-			t.sinData = t.sinData[1:]
-		} else {
-			t.sinData = append(t.sinData, float64(t.sum))
-		}
-
-		//l.Rows = listData[len(listData)%5:]
-		bc.Data = t.barchartData
-		bc.Labels = t.bcLabels
-
-		ui.Render(p, l, bc, lc2)
-
-	}
-
 	tickerCount := 1
-	draw(tickerCount, initalStat)
+	// init dashboard
+	drawDashboard(initalStat, t, dashboard, max)
 	tickerCount++
 	uiEvents := ui.PollEvents()
-	ticker := time.NewTicker(time.Second).C
+
+	ticker := time.NewTicker(time.Second * 10).C
 	for {
 		select {
 		case e := <-uiEvents:
@@ -183,7 +221,7 @@ func (t *Term) Run() {
 			}
 		case <-ticker:
 			updateParagraph(tickerCount)
-			draw(tickerCount, initalStat)
+			drawDashboard(initalStat, t, dashboard, max)
 			tickerCount++
 		}
 	}
