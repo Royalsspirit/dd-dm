94df97e0864b1c8504414198c36f77cf2739aa9d
gmajkic <gmajkic@vente-privee.com>
2020-03-17 19:46:23 +0100

    feat(test): define a few test to handle main behaviors

---
+++ internal/term/handler_dashboard_test.go
@@ -0,0 +1,48 @@
+package term
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gizak/termui/v3/widgets"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestAlert test dashboard with a threshold and a custom start date
+func TestAlert(t *testing.T) {
+	te := Term{
+		start:     time.Now().Add(-120 * time.Second),
+		threshold: 10,
+	}
+	d := dashboard{
+		pa: widgets.NewParagraph(),
+	}
+	drawAlert(&te, &d, 11)
+	assert.Contains(t, d.pa.Text, "alert", "should contain alert")
+}
+
+// TestBartChart test a barchart percentage
+func TestBartChart(t *testing.T) {
+	te := Term{
+		queue: []line{{
+			section: "/toto",
+			date:    time.Now().String(),
+		},
+		},
+		statistics: map[string]int{"/toto": 0, "/tata": 0},
+		sum:        1,
+	}
+
+	drawBarchart(&te)
+	assert.Equal(t, te.barchartData[0], float64(100), "should equal to 100")
+	te.queue = []line{{
+		section: "/tata",
+		date:    time.Now().String(),
+	},
+	}
+	te.sum = 2
+	drawBarchart(&te)
+
+	assert.Equal(t, te.barchartData[0], float64(50), "should equal to 50")
+
+}
