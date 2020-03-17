69658f7ac5510f08157d35431442c1fec988d15c
gmajkic <gmajkic@vente-privee.com>
2020-03-17 19:38:57 +0100

    feat(logfile): define a structure to handle line details

--- internal/term/handler_logfile.go
+++ internal/term/handler_logfile.go
@@ -5,35 +5,48 @@ import (
 	"regexp"
 )
 
-// Parse blbalb
+// Parse logfile file to get new line added
 func (t *Term) Parse(initialState os.FileInfo) {
 	file, err := os.Open(t.logfile)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
+	// no have previous
+	// so previous = currentState
 	if t.previousState == nil {
 		t.previousState = initialState
 	}
 	initialState, _ = os.Stat(t.logfile)
-
-	buf := make([]byte, t.previousState.Size()+(t.previousState.Size()-initialState.Size()), t.previousState.Size()+(t.previousState.Size()-initialState.Size()))
-
+	// use the file size before a trafic in log file it's observed
+	// and substract it to current file size to get a len and cap value of new lines added
+	buf := make([]byte,
+		t.previousState.Size()+(t.previousState.Size()-initialState.Size()),
+		t.previousState.Size()+(t.previousState.Size()-initialState.Size()),
+	)
+	// if the init size and the current size are different and the update date are different
 	if t.previousState.Size() != initialState.Size() && t.previousState.ModTime() != initialState.ModTime() {
 		start := t.previousState.Size()
 		file.ReadAt(buf, start)
 		t.parseLine(buf)
 	}
-
+	// new lines are managed, move on tail of file
 	t.previousState, _ = os.Stat(t.logfile)
 }
 
+// get all section found in new lines added
 func (t *Term) parseLine(b []byte) {
-	//127.0.0.1 - james [09/May/2018:16:00:39 +0000] "GET /report HTTP/1.0" 200 123
-	re := regexp.MustCompile(`/([a-z]+)`)
-	result := re.FindAllString(string(b), -1)
+	reDate := regexp.MustCompile(`(?m)\[(.+)\]`)
+	reSec := regexp.MustCompile(`\s/([a-z]+)`)
 
-	//t.sinData = append(t.sinData, float64(len(result)))
-	t.queue = append(t.queue, result...)
+	sections := reSec.FindAllString(string(b), -1)
+	dates := reDate.FindAllString(string(b), -1)
 
+	queueInfo := make([]line, len(sections))
+	for i := 0; i < len(sections); i++ {
+		queueInfo[i].section = sections[i]
+		queueInfo[i].date = dates[i]
+	}
+
+	t.queue = append(t.queue, queueInfo...)
 }
