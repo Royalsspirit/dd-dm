178e4ca9705e31808965d2712642d24ca30bf802
gmajkic <gmajkic@vente-privee.com>
2020-04-01 17:07:24 +0200

    fix(logfile): for a better efficient, use current size of data added; compile regex only once

--- internal/term/handler_logfile.go
+++ internal/term/handler_logfile.go
@@ -5,6 +5,9 @@ import (
 	"regexp"
 )
 
+var reDate = regexp.MustCompile(`(?m)\[(.+)\]`)
+var reSec = regexp.MustCompile(`\s/([a-z]+)`)
+
 // Parse logfile file to get new line added
 func (t *Term) Parse(initialState os.FileInfo) {
 	file, err := os.Open(t.logfile)
@@ -21,9 +24,10 @@ func (t *Term) Parse(initialState os.FileInfo) {
 	// use the file size before a trafic in log file it's observed
 	// and substract it to current file size to get a len and cap value of new lines added
 	buf := make([]byte,
-		t.previousState.Size()+(t.previousState.Size()-initialState.Size()),
-		t.previousState.Size()+(t.previousState.Size()-initialState.Size()),
+		initialState.Size()-t.previousState.Size(),
+		initialState.Size()-t.previousState.Size(),
 	)
+
 	// if the init size and the current size are different and the update date are different
 	if t.previousState.Size() != initialState.Size() && t.previousState.ModTime() != initialState.ModTime() {
 		start := t.previousState.Size()
@@ -36,9 +40,6 @@ func (t *Term) Parse(initialState os.FileInfo) {
 
 // get all section found in new lines added
 func (t *Term) parseLine(b []byte) {
-	reDate := regexp.MustCompile(`(?m)\[(.+)\]`)
-	reSec := regexp.MustCompile(`\s/([a-z]+)`)
-
 	sections := reSec.FindAllString(string(b), -1)
 	dates := reDate.FindAllString(string(b), -1)
 
