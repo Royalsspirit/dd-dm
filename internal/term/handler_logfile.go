0fbe1f6913fc3b31d228eb674a73e7bb1e6207c4
gmajkic <gmajkic@vente-privee.com>
2020-04-03 20:16:05 +0200

    feat(logfile): add two function which implement fsnotify and buffio as example; improve previous way of reading file

--- internal/term/handler_logfile.go
+++ internal/term/handler_logfile.go
@@ -1,8 +1,12 @@
 package term
 
 import (
+	"bufio"
+	"io"
 	"os"
 	"regexp"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 var reDate = regexp.MustCompile(`(?m)\[(.+)\]`)
@@ -10,11 +14,6 @@ var reSec = regexp.MustCompile(`\s/([a-z]+)`)
 
 // Parse logfile file to get new line added
 func (t *Term) Parse(initialState os.FileInfo) {
-	file, err := os.Open(t.logfile)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
 	// no have previous
 	// so previous = currentState
 	if t.previousState == nil {
@@ -27,15 +26,57 @@ func (t *Term) Parse(initialState os.FileInfo) {
 		initialState.Size()-t.previousState.Size(),
 		initialState.Size()-t.previousState.Size(),
 	)
-
 	// if the init size and the current size are different and the update date are different
 	if t.previousState.Size() != initialState.Size() && t.previousState.ModTime() != initialState.ModTime() {
+		file, err := os.Open(t.logfile)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
 		start := t.previousState.Size()
 		file.ReadAt(buf, start)
 		t.parseLine(buf)
 	}
 	// new lines are managed, move on tail of file
 	t.previousState, _ = os.Stat(t.logfile)
+}
+
+// ParseWithNotify ParseWithNotify
+func (t *Term) ParseWithNotify() error {
+	file, _ := os.Open(t.logfile)
+	watcher, _ := fsnotify.NewWatcher()
+	defer watcher.Close()
+	_ = watcher.Add(t.logfile)
+
+	file.Seek(0, os.SEEK_END)
+	r := bufio.NewReader(file)
+	for {
+		by, err := r.ReadBytes('\n')
+		if err != nil && err != io.EOF {
+			return err
+		}
+		t.parseLine(by)
+		if err != io.EOF {
+			continue
+		}
+		if err = waitForChange(watcher); err != nil {
+			return err
+		}
+	}
+}
+
+func waitForChange(w *fsnotify.Watcher) error {
+	for {
+		select {
+		case event := <-w.Events:
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				return nil
+			}
+		case err := <-w.Errors:
+			return err
+		}
+	}
 }
 
 // get all section found in new lines added
